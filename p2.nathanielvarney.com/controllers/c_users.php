4f8f32ef51f467540046bd768fac0bfe98de8448
Nathan <nathanielvarney@gmail.com>
2012-10-28 16:24:14 -0400

    p2 logout

--- p2.nathanielvarney.com/controllers/c_users.php
+++ p2.nathanielvarney.com/controllers/c_users.php
@@ -81,26 +81,51 @@ class users_controller extends base_controller {
 			setcookie("token", $token, strtotime('+1 year'), '/');
 			
 			# Send them to the main page - or whever you want them to go
-			Router::redirect("/");
-						
+			Router::redirect("/");			
 		}
 	
 	}
 	
 	public function logout() {
-		echo "This is the logout page";
+		# echo "This is the logout page";
+		
+		# Generate and save a new token for next login
+		$new_token = sha1(TOKEN_SALT.$this->user->email.Utils::generate_random_string());
+		
+		# Create the data array we'll use with the update method
+		# In this case, we're only updating one field, so our array only has one entry
+		$data = Array("token" => $new_token);
+		
+		# Do the update
+		DB::instance(DB_NAME)->update("users", $data, "WHERE token = '".$this->user->token."'");
+		
+		# Delete their token cookie - effectively logging them out
+		setcookie("token", "", strtotime('-1 year'), '/');
+		
+		# Send them back to the main landing page
+		Router::redirect("/");
+
 	}
 	
-	public function profile($user_name = NULL) {
+	public function profile() {
+		# If user is blank, they're not logged in, show message and don't do anything else
+		if(!$this->user) {
+			echo "Members only. <a href='/users/login'>Login</a>";
+			
+			# Return will force this method to exit here so the rest of 
+			# the code won't be executed and the profile view won't be displayed.
+			return false;
+		}
+		
 		# Setup view
 		$this->template->content = View::instance('v_users_profile');
-		$this->template->title   = "Profile";
-
+		$this->template->title   = "Profile of".$this->user->first_name;
+			
 		# Load CSS / JS
 		$client_files = Array(
-				"/css/users.css",
-				"/js/users.js",
-	            );
+			"/css/users.css",
+			"/js/users.js",
+			);
 	
         $this->template->client_files = Utils::load_client_files($client_files);  
 
