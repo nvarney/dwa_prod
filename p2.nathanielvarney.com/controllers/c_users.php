d98e68d51c519e68c92a958cde77778c2c9bf819
Nathan <nathanielvarney@gmail.com>
2012-10-28 15:35:54 -0400

    Creating Users

--- p2.nathanielvarney.com/controllers/c_users.php
+++ p2.nathanielvarney.com/controllers/c_users.php
@@ -3,7 +3,7 @@ class users_controller extends base_controller {
 
 	public function __construct() {
 		parent::__construct();
-		echo "users_controller construct called<br><br>";
+		# echo "users_controller construct called<br><br>";
 	} 
 	
 	public function index() {
@@ -11,7 +11,33 @@ class users_controller extends base_controller {
 	}
 	
 	public function signup() {
-		echo "This is the signup page";
+		# echo "This is the signup page";
+		# Setup view
+			$this->template->content = View::instance('v_users_signup');
+			$this->template->title   = "Signup";
+			
+		# Render template
+			echo $this->template;
+	}
+	
+	public function p_signup() {
+		
+		# Dump out the results of POST to see what the form submitted
+		# print_r($_POST);
+		
+		# Encrypt the password	
+		$_POST['password'] = sha1(PASSWORD_SALT.$_POST['password']);	
+		
+		# More data we want stored with the user	
+		$_POST['created'] = Time::now();
+		$_POST['modified'] = Time::now();
+		$_POST['token'] = sha1(TOKEN_SALT.$_POST['email'].Utils::generate_random_string());
+		
+		# Insert this user into the database
+		$user_id = DB::instance(DB_NAME)->insert("users", $_POST);
+		
+		# For now, just confirm they've signed up - we can make this fancier later
+		echo "You're signed up";
 	}
 	
 	public function login() {
