a44ed62327439b60e67a674bcbd7296c8d50c179
Nathan <nathanielvarney@gmail.com>
2012-10-28 16:30:02 -0400

    p2 quick and dirty test page

--- p2.nathanielvarney.com/views/v_index_index.php
+++ p2.nathanielvarney.com/views/v_index_index.php
@@ -4,3 +4,11 @@ Controller: c_index.php
 Method: index()
 View: v_index_index.php
 </pre>
+<br>
+<a href="\users\login">Login</a>
+<br>
+<a href="\users\logout">Logout</a>
+<br>
+<a href="\users\signup">Signup</a>
+<br>
+<a href="\users\profile">Profile</a>
