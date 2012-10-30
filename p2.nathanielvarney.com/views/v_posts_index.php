155551f57ccdd5b7db2310c1ffb4050bf225de5b
Nathan <nathanielvarney@gmail.com>
2012-10-29 22:19:35 -0400

    p2 user images on posts working need to clean up

--- p2.nathanielvarney.com/views/v_posts_index.php
+++ p2.nathanielvarney.com/views/v_posts_index.php
@@ -1,6 +1,8 @@
 <? foreach($posts as $post): ?>
 	
-	<h2><?=$post['first_name']?> <?=$post['last_name']?> posted:</h2>
+	<h2>
+	<img src=<?=$post['user_image_url']?> width=100 height=100>
+	<?=$post['first_name']?> <?=$post['last_name']?> posted:</h2>
 	<?=$post['content']?>
 	
 	<br><br>
