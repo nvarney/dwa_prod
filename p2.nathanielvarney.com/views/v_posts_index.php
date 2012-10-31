c0e29f147f5f850ff078a581c539138127c99b8c
Nathan <nathanielvarney@gmail.com>
2012-10-31 18:07:54 -0400

    Revert "p2 display testing... to be moved to css"
    This reverts commit bb2fc9c1de2eff3187519dbb99ced6a9b8ec69e1.

--- p2.nathanielvarney.com/views/v_posts_index.php
+++ p2.nathanielvarney.com/views/v_posts_index.php
@@ -1,7 +1,7 @@
 <? foreach($posts as $post): ?>
 	
 	<h2>
-	<img src=<?=$post['user_image_url']?> width=100 height=100 float="left">
+	<img src=<?=$post['user_image_url']?> width=100 height=100>
 	<?=$post['first_name']?> <?=$post['last_name']?> posted:</h2>
 	<?=$post['content']?>
 	
