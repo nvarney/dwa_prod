7a2dd5576a22bb312f758edc52c099987db1e84c
nvarney <nathanielvarney@gmail.com>
2012-11-03 13:23:03 -0400

    Revert "p2 trying different index page"
    This reverts commit 3ab49aca70343201110eddd65a5fab2f60d919bb.

--- p2.nathanielvarney.com/views/v_index_index.php
+++ p2.nathanielvarney.com/views/v_index_index.php
@@ -1,42 +1,20 @@
-<form float='LEFT' width='430px' method='POST' action='/users/p_login'>
-
-	Email<br>
-	<input type='text' name='email'>	
-	<br><br>
-	
-	Password<br>
-	<input type='password' name='password'>
-	<br><br>
-	
-	<? if($error): ?>
-		<div class='error'>
-			Login failed. Please double check your email and password.
-		</div>
-		<br>
-	<? endif; ?>
-
-	<input type='submit'>
-
-</form>
-
-<form float='RIGHT' width='430px' method='POST' action='/users/p_signup'>
-
-	First Name<br>
-	<input type='text' name='first_name'>
-	<br><br>
-	
-	Last Name<br>
-	<input type='text' name='last_name'>
-	<br><br>
-
-	Email<br>
-	<input type='text' name='email'>
-	<br><br>
-	
-	Password<br>
-	<input type='password' name='password'>
-	<br><br>
-	
-	<input type='submit'>
-
-</form> 
+<pre>
+Hello World!
+Controller: c_index.php
+Method: index()
+View: v_index_index.php
+</pre>
+<br>
+<a href="\users\login">Login</a>
+<br>
+<a href="\users\logout">Logout</a>
+<br>
+<a href="\users\signup">Signup</a>
+<br>
+<a href="\users\profile">Profile</a>
+<br>
+<a href="\posts">Posts</a>
+<br>
+<a href="\posts\add">Add Post</a>
+<br>
+<a href="\posts\users">Follow Users</a>
