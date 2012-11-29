0d3fd791592444006890836c4101b6b23765ae9f
nvarney <nathanielvarney@gmail.com>
2012-11-28 20:48:55 -0500

    p4 comma check

--- p4.nathanielvarney.com/index.php
+++ p4.nathanielvarney.com/index.php
@@ -24,7 +24,7 @@
 # Routing
     Router::$routes = array(
     	'/' => '/index',     # default controller when "/" is requested
-    	'proposal' => '/index/proposal'
+    	'proposal' => '/index/proposal',    # move to proposal
     );
     
 # Match requested uri to any routes and instantiate controller
