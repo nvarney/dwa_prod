50c69f208ead78302f32026d157606d26e1b59ea
nvarney <nathanielvarney@gmail.com>
2012-12-15 14:11:45 -0500

    p4 form checkpoint

--- p4.nathanielvarney.com/views/v_tickets_index.php
+++ p4.nathanielvarney.com/views/v_tickets_index.php
@@ -10,6 +10,18 @@
 			<p>There is a complete set of <a href="forms-all-mini.html">mini-sized</a> form elements which are useful for toolbars or tighter spaces. <a href="forms-all-compare.html">Compare mini and normal</a> form elements side-by-side.</p>
 
 			<div data-role="fieldcontain">
+			    <fieldset data-role="controlgroup" data-type="horizontal" data-theme="a">
+			     	<legend>Ticket Type:</legend>
+			         	<label for="setup">PC Setup</label>
+			         	<input type="radio" name="ticket-type" id="setup" value="pc-setup" checked="checked" />
+			         	<label for="virus">Virus Removal</label>
+			         	<input type="radio" name="ticket-type" id="virus" value="virus" />
+			         	<label for="other-issue">Other Issue</label>
+			         	<input type="radio" name="ticket-type" id="other-issue" value="other-issue" />
+			    </fieldset>
+			</div>
+
+			<div data-role="fieldcontain">
 	         <label for="first_name">First Name:</label>
 	         <input type="text" name="first_name" id="first_name" class="general_input" value=""  />
 			</div>
@@ -25,14 +37,44 @@
 			</div>
 			
 			<div data-role="fieldcontain">
-	         <label for="first_name">First Name:</label>
-	         <input type="text" name="first_name" id="first_name" class="general_input" value=""  />
+	         <label for="phone">Telephone:</label>
+	         <input type="text" name="phone" id="phone" class="general_input" value=""  />
 			</div>
 			
+			<div data-role="fieldcontain">
+				<label for="model" class="select">Computer Model:</label>
+				<select name="model" id="model" data-native-menu="false" data-theme="a">
+					<option>Choose Computer Model</option>
+					<optgroup label="Dell">
+						<option value="dell-optiplex" selected>Optiplex</option>
+						<option value="dell-latitude">Latitude</option>
+						<option value="dell-precision">Precision</option>
+					</optgroup>
+					<optgroup label="Lenovo/IBM">
+						<option value="lenovo-thinkpad">ThinkPad</option>
+						<option value="lenovo-thinkstation">ThinkStation</option>
+						<option value="lenovo-thinkcentre">ThinkCentre</option>
+					</optgroup>
+					<optgroup label="Apple">
+						<option value="apple-macbookpro">MacBook Pro</option>
+						<option value="apple-macbookair">MacBook Air</option>
+						<option value="apple-imac">iMac</option>
+						<option value="apple-macmini">Mac Mini</option>
+						<option value="apple-macpro">Mac Pro</option>
+					</optgroup>
+				</select>
+			</div>
 			
+			<div data-role="fieldcontain">
+	         <label for="serial">Computer Serial #:</label>
+	         <input type="text" name="serial" id="serial" class="general_input" value=""  />
+			</div>
 			
-			
-			
+			<div data-role="fieldcontain">
+	         <label for="notes">Notes:</label>
+	         <textarea name="notes" id="notes" class="general_input" value=""></textarea>
+			</div>
+						
 		</form>
 		</div>
 </div>
