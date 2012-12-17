3645b26266b3e735d70aaced4051ce923f4633d4
nvarney <nathanielvarney@gmail.com>
2012-12-16 23:52:01 -0500

    p4 validation script working

--- p4.nathanielvarney.com/js/p4.js
+++ p4.nathanielvarney.com/js/p4.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {	
-
+	$('form').validate();
+	
+	$('form').live('click focus mouseover', function() {
+		$(this).validate();
+	});
 
 });
--- p4.nathanielvarney.com/js/p4_tickets.js
+++ p4.nathanielvarney.com/js/p4_tickets.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {	
-	function onPageInit() {
-		$("#ticket-form").validate();
-	});
+	
 });
