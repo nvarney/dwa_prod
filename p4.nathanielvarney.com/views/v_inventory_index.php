8a4b9dac0740bf9bf6d6c4017cf65e8597cc49fb
nvarney <nathanielvarney@gmail.com>
2012-12-18 21:54:28 -0500

    Revert "p4 fixing errors"
    This reverts commit bf0129e09435d7f22ebc82272c6f853c79ef69fe.

--- p4.nathanielvarney.com/views/v_inventory_index.php
+++ p4.nathanielvarney.com/views/v_inventory_index.php
@@ -1,21 +1,17 @@
 <div data-role="content" data-theme="b" data-ajax="false">
-
-	<!-- List computers whose location is currently set to the Helpdesk -->
 	<h2>Active Computers</h2>
 		<p>These are the computers currently at the helpdesk</p>
            <ul data-role="listview" data-filter="true" data-inset="true" data-split-icon="delete" data-theme="a" data-split-theme="b">
 			<? foreach($active as $active): ?>
-			<li><a href="#"> <!--link to nothing because links looked better in the list -->
+			<li><a href="#">
 				<h2><?=$active['name']?></h2>
 				<p><?=$active['model']?>  <?=$active['serial']?> </p>
 				<p><?=$active['ticket_id']?> <? echo date("F j, Y", $active['modified']); ?> </p>
-				<!-- Links to function for changing the computer's location in the database -->
 				</a><a href='/inventory/return_pc/<?=$active['serial']?>'>Return Computer</a>
 			</li>
 			<? endforeach; ?>
 		</ul>	
 
-	<!-- List computers where location is currently set to Other -->
 	<h2>Returned Computers</h2>
 		<p>These are the computers have recently been returned to customers</p>
             <ul data-role="listview" data-filter="true" data-inset="true" data-theme="a" data-split-theme="b">
@@ -28,10 +24,4 @@
 			</li>
 			<? endforeach; ?>
 		</ul>
-		
-	<!-- Logout button -->
-	<div id="logoutButton">
-		<a href="/tickets/logout" data-role="button" data-inline="true" data-theme="b">Logout</a>
-	</div>
-	
 </div>
