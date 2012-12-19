44288eb0750460a0680528f7a02a490cf03e1d2d
nvarney <nathanielvarney@gmail.com>
2012-12-18 22:05:08 -0500

    p4 update to popup login working

--- p4.nathanielvarney.com/controllers/c_index.php
+++ p4.nathanielvarney.com/controllers/c_index.php
@@ -30,38 +30,6 @@ class index_controller extends base_controller {
 
 	}
 	
-	public function mail() {
-
-		# Build a multi-dimension array of recipients of this email
-		$to[] = Array("name" => "Nathan", "email" => "mrplasma@gmail.com");
-		
-		# Build a single-dimension array of who this email is coming from
-		# note it's using the constants we set in the configuration above)
-		$from = Array("name" => APP_NAME, "email" => APP_EMAIL);
-		
-		# Subject
-		$subject = "Test email4";
-		
-		# You can set the body as just a string of text
-		$body = "Hi Judy, this is just a message to confirm your registration at JavaBeans.com";
-		
-		# OR, if your email is complex and involves HTML/CSS, you can build the body via a View just like we do in our controllers
-		# $body = View::instance('e_users_welcome');
-		
-		# Build multi-dimension arrays of name / email pairs for cc / bcc if you want to 
-		$cc  = "";
-		$bcc = "";
-		
-		# With everything set, send the email
-		
-		if(!$email = Email::send($to, $from, $subject, $body, true, $cc, $bcc)) {
-  			echo "Mailer Error: " . $mail->ErrorInfo;
-		} else {
-  			echo "Message sent!";
-		}
-	
-	}
-	
 	/*-------------------------------------------------------------------------------------------------
 	Using login code from p2. In real production, this would be linked in to LDAP, but for this project
 	I am just using a local database.
@@ -77,7 +45,7 @@ class index_controller extends base_controller {
 		# Retrieve the token if it's available
 		$q = "SELECT token 
 			FROM users 
-			WHERE email = '".$_POST['email']."' 
+			WHERE email = '".$_POST['emailUsername']."' 
 			AND password = '".$_POST['password']."'";
 		
 		$token = DB::instance(DB_NAME)->select_field($q);	
